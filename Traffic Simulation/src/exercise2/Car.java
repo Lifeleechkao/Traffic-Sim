606bb0ee883e5b70045144b98cf17a8f58e089e5
Lifeleechkao <Shadowpeach97@gmail.com>
2016-02-10 16:45:47 -0700

    Week 3 EastBound Turns

--- Traffic Simulation/src/exercise2/Car.java
+++ Traffic Simulation/src/exercise2/Car.java
@@ -16,119 +16,159 @@ public class Car extends Actor implements IntersectionListener {
 	private int stopSpeed = 0;
 	private int fullSpeed = 5;
 	private int currentSpeed = fullSpeed;
-	private String[] carImages = {"images\\topCarBlue.png","images\\topCarRed.png","images\\topCarPurple.png","images\\topCarYellow.png"};
-private static enum state{
-	APPROACHING,
-	AT_INTERSECTION,
-	LEAVING;
-}
-public Car(Direction d){
-	this.d = d;
-	int colourRole = randomColours.nextInt(carImages.length);
-	GreenfootImage image = new GreenfootImage(carImages[colourRole]);
-	this.setImage(image);
-}
+	private String[] carImages = { "images\\topCarBlue.png", "images\\topCarRed.png", "images\\topCarPurple.png",
+			"images\\topCarYellow.png" };
+	private Random r = new Random();
 
-public void act(){
-	move(currentSpeed);
-	if(intersection != null){
-	if(this.getRotation() == 0){
-		t = intersection.getLightArray(2);
-		
-	}else if(this.getRotation() == 90){
-		t = intersection.getLightArray(0);
-		
-	}else if(this.getRotation() == 180){
-		t = intersection.getLightArray(3);
-		
-	}else if(this.getRotation() == 270){
-		t = intersection.getLightArray(1);
-		
+	private static enum state {
+		APPROACHING, AT_INTERSECTION, LEAVING;
 	}
+
+	public Car(Direction d) {
+		this.d = d;
+		int colourRole = randomColours.nextInt(carImages.length);
+		GreenfootImage image = new GreenfootImage(carImages[colourRole]);
+		this.setImage(image);
 	}
-	if(t != null){
-	switch(carState){
-	case APPROACHING : if(t.getColor() == TrafficLights.Color.GREEN){
-		speedUp();
-	}else{
-		slowDown();
-		
+
+	public boolean isAtEnd() {
+		return ((this.d == Direction.EAST && this.getX() > 0)
+				|| (this.d == Direction.WEST && this.getX() < TrafficWorld.WIDTH)
+				|| (this.d == Direction.NORTH && this.getX() > 0)
+				|| (this.d == Direction.SOUTH && this.getX() < TrafficWorld.HEIGHT));
 	}
-	break;
-	case AT_INTERSECTION :
-		if(currentSpeed <= slowSpeed){
-		currentSpeed = stopSpeed;
-	}else{
-		currentSpeed = fullSpeed;
-	}
-		if(currentSpeed == 0 && t.getColor() == TrafficLights.Color.GREEN) {
-			currentSpeed = fullSpeed;
-		}
-		
-	break;
-	case LEAVING : 
-		speedUp();
-	
-	break;
-	}
-	
-	if(isAtEdge()){
-		switch(this.d){
-			case EAST: 
-				 setLocation(0, getY());
-				break;
-			case WEST:
-				setLocation(TrafficWorld.WIDTH, getY());
-				break;
-			case NORTH:
-				setLocation(getX(), TrafficWorld.HEIGHT);
-				break;
-			case SOUTH:
-				setLocation(getX(), 0);
-				break;
-		
-	
+
+	public void act() {
+		move(currentSpeed);
+		if (intersection != null) {
+			if (this.getRotation() == 0) {
+				t = intersection.getLightArray(2);
+
+			} else if (this.getRotation() == 90) {
+				t = intersection.getLightArray(0);
+
+			} else if (this.getRotation() == 180) {
+				t = intersection.getLightArray(3);
+
+			} else if (this.getRotation() == 270) {
+				t = intersection.getLightArray(1);
+
+			}
 		}
 
+		if (t != null) {
+			switch (carState) {
+			case APPROACHING:
+				if (t.getColor() == TrafficLights.Color.GREEN) {
+					speedUp();
+				} else {
+					slowDown();
+
+				}
+				break;
+			case AT_INTERSECTION:
+				if (currentSpeed <= slowSpeed) {
+					currentSpeed = stopSpeed;
+				} else {
+					currentSpeed = fullSpeed;
+				}
+				if (currentSpeed == 0 && t.getColor() == TrafficLights.Color.GREEN) {
+					currentSpeed = fullSpeed;
+				}
+
+				break;
+			case LEAVING:
+				speedUp();
+
+				break;
+			}
+
+			if (isAtEdge()) {
+				switch (this.d) {
+				case EAST:
+					setLocation(0, getY());
+					break;
+				case WEST:
+					setLocation(TrafficWorld.WIDTH, getY());
+					break;
+				case NORTH:
+					setLocation(getX(), TrafficWorld.HEIGHT);
+					break;
+				case SOUTH:
+					setLocation(getX(), 0);
+					break;
+
+				}
+
+			}
+			if (this.isAtEdge() && isAtEnd()) {
+				getWorld().removeObject(this);
+			}
+		}
+		move(currentSpeed);
+		if (this.carState == state.AT_INTERSECTION) {
+			Random b = new Random();
+			int randomTurn = b.nextInt(25);
+			if (randomTurn == 1 && d == Direction.WEST) {
+
+			}
+		}
+		if (this.d.ordinal() == d.EAST.ordinal()) {
+			if (this.getX() == intersection.getX() - Roads.ROAD_WIDTH / 4) {
+				int random = r.nextInt(30);
+				if (random == 25) {
+					this.turn(90);
+					this.d = d.SOUTH;
+				}
+			}
+		}
+		if (this.d.ordinal() == d.EAST.ordinal()) {
+			if (this.getX() == intersection.getX() + Roads.ROAD_WIDTH / 4) {
+				int random = r.nextInt(30);
+				if (random == 20) {
+					this.turn(270);
+					this.d = d.NORTH;
+				}
+			}
+		}
 	}
+
+	public void speedUp() {
+		if (currentSpeed < fullSpeed) {
+			currentSpeed++;
+		}
 	}
-	move(currentSpeed);
-}
-public void speedUp(){
-	if(currentSpeed < fullSpeed){
-		currentSpeed++;
+
+	public void slowDown() {
+		if (currentSpeed > slowSpeed) {
+			currentSpeed--;
+		}
 	}
-}
-public void slowDown(){
-	if(currentSpeed > slowSpeed){
-		currentSpeed--;
+	// public void setLightColor(TrafficLights.Color lightColor) {
+	// this.lightColor = lightColor;
+	// }
+
+	@Override
+	public void isApproaching(Intersection i) {
+		// TODO Auto-generated method stub
+
+		intersection = i;
+		carState = state.APPROACHING;
+
 	}
-}
-//	public void setLightColor(TrafficLights.Color lightColor) {
-//		this.lightColor = lightColor;
-//	}
 
-@Override
-public void isApproaching(Intersection i) {
-	// TODO Auto-generated method stub
-	
-	intersection = i;
-	carState = state.APPROACHING;
+	@Override
+	public void isAtIntersection(Intersection i) {
+		// TODO Auto-generated method stub
 
+		intersection = i;
+		carState = state.AT_INTERSECTION;
 
-}
+	}
 
-@Override
-public void isAtIntersection(Intersection i) {
-	// TODO Auto-generated method stub
-	
-	intersection = i;
-	carState = state.AT_INTERSECTION;
-}
-
-@Override
-public void isLeavingIntersection(Intersection i) {
-	// TODO Auto-generated method stub
+	@Override
+	public void isLeavingIntersection(Intersection i) {
+		// TODO Auto-generated method stub
 
 		intersection = i;
 		carState = state.LEAVING;
--- Traffic Simulation/src/exercise2/TrafficWorld.java
+++ Traffic Simulation/src/exercise2/TrafficWorld.java
@@ -17,6 +17,43 @@ public class TrafficWorld extends World {
 	private static Roads[] HorizontalRoads = new Roads[NUMOFHORIZONTALROADS];
 	private static Roads[] VerticalRoads = new Roads[NUMOFVERTICALROADS];
 	private static Random r = new Random();
+
+	public void act(){
+		Direction d;
+		int random = r.nextInt(90);
+		int horizontalPlacement = r.nextInt(NUMOFHORIZONTALROADS);
+		int verticalPlacement = r.nextInt(NUMOFVERTICALROADS);
+		if(random == 1){
+			Car car = null;
+			
+		int carDirection = r.nextInt(4);
+		if(carDirection == 1){
+			car = new Car(Direction.EAST);
+			this.addObject(car, 0, 0);
+			car.setLocation(0, HorizontalRoads[horizontalPlacement].getY() + Roads.ROAD_WIDTH/4);
+		}else if(carDirection == 2){
+			 car = new Car(Direction.WEST);
+			 car.setRotation(180);
+			 this.addObject(car, 0, 0);
+			 car.setLocation(WIDTH,  HorizontalRoads[horizontalPlacement].getY() - Roads.ROAD_WIDTH/4);
+		}else if(carDirection == 3){
+			car = new Car(Direction.NORTH);
+			car.setRotation(270);
+			this.addObject(car, 0, 0);
+			car.setLocation(VerticalRoads[verticalPlacement].getX() + Roads.ROAD_WIDTH/4, HEIGHT);
+		}else if(carDirection == 0){
+			car = new Car(Direction.SOUTH);
+			car.setRotation(90);
+			this.addObject(car, 0, 0);
+			car.setLocation(VerticalRoads[verticalPlacement].getX() - Roads.ROAD_WIDTH/4, 0);
+		}
+		
+		
+	
+		}
+	
+	
+		}
 	public TrafficWorld(){
 		super(WIDTH, HEIGHT, CELL_SIZE);
 		GreenfootImage background = this.getBackground();
@@ -39,27 +76,27 @@ public class TrafficWorld extends World {
 			addObject(VerticalRoads[i],(((VERTICALGAPSIZE +Roads.ROAD_WIDTH)*i)+Roads.ROAD_WIDTH/2), HEIGHT/2 );
 		}
 
-		for(int i = 0; i< NUMOFHORIZONTALROADS; i++){
-			Car car = new Car(Direction.EAST);
-
-			this.addObject(car, r.nextInt(WIDTH)  , ((Roads.ROAD_WIDTH/4)*3) + (( HORIZONTALGAPSIZE + Roads.ROAD_WIDTH) * i));
-		}
-		for(int i = 0; i < NUMOFHORIZONTALROADS; i++){
-			Car car =  new Car(Direction.WEST);
-			car.setRotation(180);
-			this.addObject(car, r.nextInt(WIDTH)  , ((Roads.ROAD_WIDTH/4 ) + (( HORIZONTALGAPSIZE + Roads.ROAD_WIDTH) * i)));
-
-		}
-		for(int i =  0; i < NUMOFVERTICALROADS; i++ ){
-			Car car = new Car(Direction.NORTH);
-			car.setRotation(270);
-			this.addObject(car, ((Roads.ROAD_WIDTH/4)*3) + (( VERTICALGAPSIZE + Roads.ROAD_WIDTH) * i)  , r.nextInt(HEIGHT));
-		}
-		for(int i =  0; i < NUMOFVERTICALROADS; i++ ){
-			Car car = new Car(Direction.SOUTH);
-			car.setRotation(90);
-			this.addObject(car,  ((Roads.ROAD_WIDTH/4 ) + (( VERTICALGAPSIZE + Roads.ROAD_WIDTH) * i)) , r.nextInt(HEIGHT));
-		}
+//		for(int i = 0; i< NUMOFHORIZONTALROADS; i++){
+//			Car car = new Car(Direction.EAST);
+//
+//			this.addObject(car, r.nextInt(WIDTH)  , ((Roads.ROAD_WIDTH/4)*3) + (( HORIZONTALGAPSIZE + Roads.ROAD_WIDTH) * i));
+//		}
+//		for(int i = 0; i < NUMOFHORIZONTALROADS; i++){
+//			Car car =  new Car(Direction.WEST);
+//			car.setRotation(180);
+//			this.addObject(car, r.nextInt(WIDTH)  , ((Roads.ROAD_WIDTH/4 ) + (( HORIZONTALGAPSIZE + Roads.ROAD_WIDTH) * i)));
+//
+//		}
+//		for(int i =  0; i < NUMOFVERTICALROADS; i++ ){
+//			Car car = new Car(Direction.NORTH);
+//			car.setRotation(270);
+//			this.addObject(car, ((Roads.ROAD_WIDTH/4)*3) + (( VERTICALGAPSIZE + Roads.ROAD_WIDTH) * i)  , r.nextInt(HEIGHT));
+//		}
+//		for(int i =  0; i < NUMOFVERTICALROADS; i++ ){
+//			Car car = new Car(Direction.SOUTH);
+//			car.setRotation(90);
+//			this.addObject(car,  ((Roads.ROAD_WIDTH/4 ) + (( VERTICALGAPSIZE + Roads.ROAD_WIDTH) * i)) , r.nextInt(HEIGHT));
+//		}
 		for(int i = 0; i < HorizontalRoads.length; i++){
 			for(int j = 0; j < VerticalRoads.length; j++){
 				Intersection intersection = new Intersection();
